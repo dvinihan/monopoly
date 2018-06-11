10ce2e2fc1ec5ac510d595007d6e75a15f9402bc
Daniel Vinitsky <daniel.vinitsky@gmail.com>
2018-06-11 14:41:20 -0500

    test db connection

--- index.js
+++ index.js
@@ -9,33 +9,29 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-
-
-app.get('/db', async (req, res) => {
-  try {
-    var con = await mysql.createConnection({
-      host: "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-      user: "uoz4qxsooap10l9c",
-      password: "ivq70bw6vnhss8e1"
-    });
-
-    con.connect(err => {
-      if (err) throw err;
-      console.log("Connected!");
-
-      var sql = "SELECT * FROM rooms";
-      con.query(sql, (err, result, fields) => {
-        if (err) throw err;
-        res.render('pages/db', result);
-        console.log("Result: " + result);
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  .get('/db', async (req, res) => {
+    try {
+      var con = await mysql.createConnection({
+        host: "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "uoz4qxsooap10l9c",
+        password: "ivq70bw6vnhss8e1"
       });
-    });
-  } catch (err) {
-    console.log(err);
-    res.send("Error " + err);
-  }
 
-});
+      con.connect(err => {
+        if (err) throw err;
+        console.log("Connected!");
+
+        var sql = "SELECT * FROM rooms";
+        con.query(sql, (err, result, fields) => {
+          if (err) throw err;
+          res.render('pages/db', result);
+          console.log("Result: " + result);
+        });
+      });
+    } catch (err) {
+      console.log(err);
+      res.send("Error " + err);
+    }
+  });
 
