a6c10566188101800f58d21fa71350a34fd3cfb9
Daniel Vinitsky <daniel.vinitsky@gmail.com>
2018-06-13 13:01:47 -0500

    Add team and room edit pages

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 var mysql = require('mysql');
 
+const app = express();
+
+//create connection to mysql db
 try {
   var con = mysql.createConnection({
     host: "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
@@ -15,38 +18,81 @@ try {
   res.send("Error " + err);
 }
 
+//connect to mysql db
 con.connect(err => {
   if (err) throw err;
   console.log("Connected!");
 
-  express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => {
+  app.use(express.static(path.join(__dirname, 'public')));
 
-      con.query("SELECT * FROM rooms", (err, result) => {
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+
+  app.get('/', (req, res) => {
+
+    con.query("SELECT * FROM rooms", (err, result) => {
+      if (err) throw err;
+      var rooms = result;
+
+      con.query("SELECT * FROM teams", (err, result) => {
         if (err) throw err;
-        var rooms = result;
+        var teams = result;
 
-        con.query("SELECT * FROM teams", (err, result) => {
-          if (err) throw err;
-          var teams = result;
+        res.render('pages/index', { rooms: rooms, teams: teams });
 
-          res.render('pages/index', { rooms: rooms, teams: teams });
-
-        });
       });
-    })
-    .get('/signup', (req, res) => {
-      res.render('pages/signup', {});
-    })
-    .get('/admin', (req, res) => {
-      res.render('pages/admin', {});
-    })
-    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+    });
+  });
 
-})
+  app.get('/signup', (req, res) => {
+    res.render('pages/signup', {});
+  });
+
+  app.get('/admin', (req, res) => {
+    con.query("SELECT * FROM rooms", (err, result) => {
+      if (err) throw err;
+      var rooms = result;
+
+      con.query("SELECT * FROM teams", (err, result) => {
+        if (err) throw err;
+        var teams = result;
+
+        res.render('pages/admin', { rooms: rooms, teams: teams });
+      });
+    });
+
+  });
+
+  app.get('/team-submit', (req, res) => {
+    //add to sql db
+    con.query("INSERT INTO teams (name) VALUES ('" + req.query.teamName + "')", (err, result) => {
+      if (err) throw err;
+      //render signup landing page
+      res.render('pages/teamSubmit', { teamName: req.query.teamName });
+    });
+  });
+
+  app.get('/teamEdit', (req, res) => {
+    con.query("SELECT * FROM teams WHERE id=" + req.query.id, (err, result) => {
+      if (err) throw err;
+      var team = result[0];
+
+      res.render('pages/teamEdit', { team: team });
+    });
+
+  });
+
+  app.get('/roomEdit', (req, res) => {
+    con.query("SELECT * FROM rooms WHERE id=" + req.query.id, (err, result) => {
+      if (err) throw err;
+      var room = result[0];
+
+      res.render('pages/roomEdit', { room: room });
+    });
+
+  });
 
 
-
+  app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+});
+  //sql connection closed
