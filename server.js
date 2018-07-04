80b468743255ee787676d78855a3efbe04b9ead5
dvinitsky <daniel.vinitsky@gmail.com>
2018-07-04 00:13:52 -0500

    commented out apostrophe middlware

--- server.js
+++ server.js
@@ -72,6 +72,7 @@ con.connect(err => {
 
   //Apostrophe check Middleware
   app.use((req, res, next) => {
+    /*
     for (const key in req.body) {
       let value = req.body[key];
       if (value.trim() === "") {
@@ -87,143 +88,129 @@ con.connect(err => {
         req.body[key] = newString;
       }
     };
+    */
     next();
   });
 
 
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'ejs');
+  app.use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs');
 
   /////////////   ROUTES    //////////////////////
 
   app.get('/', (req, res) => {
     res.render('pages/index', { teams: req.teams });
-  });
-
-  app.get('/login', (req, res) => {
-    res.render('pages/login', { error: false });
-  });
-
-  app.post('/login', (req, res) => {
-    let user = req.users.find(user => {
-      return user.username === req.body.username;
-    });
-    if (user && user.password === req.body.password) {
-      res.redirect('/admin');
-    }
-    else {
-      res.render('pages/login', { error: true });
-    }
-  });
-
-  app.get('/admin', (req, res) => {
-    res.render('pages/admin', { rooms: req.rooms, teams: req.teams })
   })
-
-  app.get('/signup', (req, res) => {
-    res.render('pages/signup', { error: false });
-  });
-
-  app.post('/signup', (req, res) => {
-    //check if the name is already taken
-    let exists = false;
-    for (let i = 0; i < req.teams.length; i++) {
-      if (req.teams[i].name === req.body.teamName) {
-        exists = true;
-      }
-    }
-
-    if (exists) {
-      res.render('pages/signup', { error: true });
-    } else {
-      con.query(`INSERT INTO teams (name) VALUES ('${req.body.teamName}')`, (err) => {
-        if (err) throw err;
-        res.redirect('/');
+    .get('/login', (req, res) => {
+      res.render('pages/login', { error: false });
+    })
+    .post('/login', (req, res) => {
+      let user = req.users.find(user => {
+        return user.username === req.body.username;
       });
-
-    }
-  });
-
-  app.get('/teamEdit', (req, res) => {
-    let team = req.teams.find(team => team.id == req.query.id);
-    res.render('pages/teamEdit', { team: team });
-  });
-
-  app.post('/teamEdit', (req, res) => {
-    con.query(`UPDATE teams SET name = '${req.body.name}', score = '${req.body.score}' WHERE id=${req.body.id}`, (err) => {
-      if (err) throw err;
-      res.redirect('/admin');
-    });
-  });
-
-  app.get('/roomEdit', (req, res) => {
-    let room = req.rooms.find(room => room.id == req.query.id);
-    res.render('pages/roomEdit', { room: room, teams: req.teams });
-  });
-
-  app.post('/roomEdit', (req, res) => {
-    if (!req.body.name) {
-      res.render('pages/error', { rooms: req.rooms, teams: req.teams });
-    } else {
-      let teamId = req.teams.find(team => team.name === req.body.teamName).id;
-
-      con.query(`UPDATE rooms SET name = '${req.body.name}', time = '${req.body.time}', teamId = '${teamId}' WHERE id = '${req.body.id}'`, (err) => {
-        if (err) throw err;
+      if (user && user.password === req.body.password) {
         res.redirect('/admin');
-      });
-    }
-  });
-
-  app.get('/roomAdd', (req, res) => {
-    res.render('pages/roomAdd', { teams: req.teams });
-  });
-
-  app.post('/roomAdd', (req, res) => {
-    if (!req.body.name) {
-      res.render('pages/error', { rooms: req.rooms, teams: req.teams });
-    } else {
+      }
+      else {
+        res.render('pages/login', { error: true });
+      }
+    })
+    .get('/admin', (req, res) => {
+      res.render('pages/admin', { rooms: req.rooms, teams: req.teams })
+    })
+    .get('/signup', (req, res) => {
+      res.render('pages/signup', { error: false });
+    })
+    .post('/signup', (req, res) => {
+      //check if the name is already taken
       let exists = false;
-      for (let i = 0; i < req.rooms.length; i++) {
-        if (req.rooms[i].name === req.body.name) {
+      for (let i = 0; i < req.teams.length; i++) {
+        if (req.teams[i].name === req.body.teamName) {
           exists = true;
         }
       }
 
       if (exists) {
+        res.render('pages/signup', { error: true });
+      } else {
+        con.query(`INSERT INTO teams (name) VALUES ('${req.body.teamName}')`, (err) => {
+          if (err) throw err;
+          res.redirect('/');
+        });
+
+      }
+    })
+    .get('/teamEdit', (req, res) => {
+      let team = req.teams.find(team => team.id == req.query.id);
+      res.render('pages/teamEdit', { team: team });
+    })
+    .post('/teamEdit', (req, res) => {
+      con.query(`UPDATE teams SET name = '${req.body.name}', score = '${req.body.score}' WHERE id=${req.body.id}`, (err) => {
+        if (err) throw err;
+        res.redirect('/admin');
+      });
+    })
+    .get('/roomEdit', (req, res) => {
+      let room = req.rooms.find(room => room.id == req.query.id);
+      res.render('pages/roomEdit', { room: room, teams: req.teams });
+    })
+    .post('/roomEdit', (req, res) => {
+      if (!req.body.name) {
         res.render('pages/error', { rooms: req.rooms, teams: req.teams });
       } else {
-        con.query(`INSERT INTO rooms (name, time, teamId) VALUES('${req.body.name}', '${req.body.time}', '${req.body.teamId}')`, (err) => {
+        let teamId = req.teams.find(team => team.name === req.body.teamName).id;
+
+        con.query(`UPDATE rooms SET name = '${req.body.name}', time = '${req.body.time}', teamId = '${teamId}' WHERE id = '${req.body.id}'`, (err) => {
           if (err) throw err;
           res.redirect('/admin');
         });
       }
-    }
-  });
+    })
+    .get('/roomAdd', (req, res) => {
+      res.render('pages/roomAdd', { teams: req.teams });
+    })
+    .post('/roomAdd', (req, res) => {
+      if (!req.body.name) {
+        res.render('pages/error', { rooms: req.rooms, teams: req.teams });
+      } else {
+        let exists = false;
+        for (let i = 0; i < req.rooms.length; i++) {
+          if (req.rooms[i].name === req.body.name) {
+            exists = true;
+          }
+        }
 
-  app.get('/roomDelete', (req, res) => {
-    res.render('pages/roomDelete', { rooms: req.rooms, teams: req.teams });
-  });
+        if (exists) {
+          res.render('pages/error', { rooms: req.rooms, teams: req.teams });
+        } else {
+          con.query(`INSERT INTO rooms (name, time, teamId) VALUES('${req.body.name}', '${req.body.time}', '${req.body.teamId}')`, (err) => {
+            if (err) throw err;
+            res.redirect('/admin');
+          });
+        }
+      }
+    })
+    .get('/roomDelete', (req, res) => {
+      res.render('pages/roomDelete', { rooms: req.rooms, teams: req.teams });
+    })
+    .post('/roomDelete', (req, res) => {
+      con.query(`DELETE FROM rooms WHERE id = '${req.body.id}'`, (err) => {
+        if (err) throw err;
+        res.redirect('/admin');
+      });
+    })
+    .get('/teamDelete', (req, res) => {
+      res.render('pages/teamDelete', { rooms: req.rooms, teams: req.teams });
+    })
+    .post('/teamDelete', (req, res) => {
+      con.query(`UPDATE rooms SET teamId ='0' WHERE teamId = ${req.body.id}`);
 
-  app.post('/roomDelete', (req, res) => {
-    con.query(`DELETE FROM rooms WHERE id = '${req.body.id}'`, (err) => {
-      if (err) throw err;
-      res.redirect('/admin');
+      con.query(`DELETE FROM teams WHERE id = '${req.body.id}'`, (err) => {
+        if (err) throw err;
+        res.redirect('/admin');
+      });
     });
-  });
-
-  app.get('/teamDelete', (req, res) => {
-    res.render('pages/teamDelete', { rooms: req.rooms, teams: req.teams });
-  });
-
-  app.post('/teamDelete', (req, res) => {
-    con.query(`UPDATE rooms SET teamId ='0' WHERE teamId = ${req.body.id}`);
-
-    con.query(`DELETE FROM teams WHERE id = '${req.body.id}'`, (err) => {
-      if (err) throw err;
-      res.redirect('/admin');
-    });
-  });
 
   /////////////////////////////////////////////////////////////
 
